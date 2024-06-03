4cc47e33876bd878929518d23b7d3fb117dc8fc7
geonho lee <lgh8079@gmail.com>
2024-06-03 14:52:52 +0900

    Initial Commit

---
+++ src/main/java/aespa/groovymap/GroovymapApplication.java
@@ -0,0 +1,13 @@
+package aespa.groovymap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovymapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovymapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aespa/groovymap/GroovymapApplicationTests.java
@@ -0,0 +1,13 @@
+package aespa.groovymap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovymapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
