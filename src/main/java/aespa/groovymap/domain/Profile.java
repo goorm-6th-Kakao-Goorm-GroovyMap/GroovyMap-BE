9ba87a9add0307d7c0d33e7c72e7cee889b6428b
YJK-dev <90750483+YJK-dev@users.noreply.github.com>
2024-06-18 14:16:49 +0900

    Feat: Profile 엔티티 추가
    - id (식별자)
    - member (Member 엔티티와의 ManyToOne 관계)
    - nickname (닉네임)
    - region (지역)
    - category (카테고리, Enum 타입)
    - introduction(자기소개)와 profileImage(프로필 이미지) 필드는 추후 구현

---
+++ src/main/java/aespa/groovymap/domain/Profile.java
@@ -0,0 +1,41 @@
+package aespa.groovymap.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    private String nickname;
+
+    private String region;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    // 자기소개, 프로필 이미지 필드 추후 구현
+}
