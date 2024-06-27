c60d0c46cc4876e4abd5b3908ce7de7ef3a360d9
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-06-27 11:51:52 +0900

    Fix : 마이 페이지 게시글 작성 요청 컨트롤러 수정
    마이 페이지 게시글 작성 요청 컨트롤러 수정
    form-data로 요청 오면 @RequestBody로 dto 받을 수 없음
    @ModelAttribute로 받아야 함
    파일 전송하고 하면 @ModelAttribute로 받아야 함

--- src/main/java/aespa/groovymap/mypage/controller/MyPagePhotoController.java
+++ src/main/java/aespa/groovymap/mypage/controller/MyPagePhotoController.java
@@ -10,9 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
@@ -32,7 +32,7 @@ public class MyPagePhotoController {
     @PostMapping("/mypage/photo/write")
     public ResponseEntity writeMyPagePhoto(
             @SessionAttribute(name = SessionConstants.MEMBER_ID, required = false) Long memberId,
-            @RequestBody MyPagePhotoWriteDto myPagePhotoWriteDto) throws IOException {
+            @ModelAttribute MyPagePhotoWriteDto myPagePhotoWriteDto) throws IOException {
         log.info("마이 페이지 게시물 작성 요청 : {}", memberId);
         if (memberId != null) {
             myPagePhotoService.writeMyPagePhoto(myPagePhotoWriteDto, memberId);
