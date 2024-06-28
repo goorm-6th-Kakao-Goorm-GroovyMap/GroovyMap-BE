42eb04b15f4ee0b0a3b638dbe330615e537c4814
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-06-28 21:30:57 +0900

    Feat : 마이 페이지 게시글 부가 기능 따로 뽑기
    마이 페이지 게시글의 부가 기능인 좋아요, 댓글 기능 따로 뽑음
    이것들 처리하는 컨트롤러, 서비스 만들어 따로 뽑아 놓음

--- src/main/java/aespa/groovymap/mypage/controller/MyPagePhotoController.java
+++ src/main/java/aespa/groovymap/mypage/controller/MyPagePhotoController.java
@@ -2,7 +2,6 @@ package aespa.groovymap.mypage.controller;
 
 import aespa.groovymap.config.SessionConstants;
 import aespa.groovymap.mypage.dto.MyPagePhoto.MyPageOnePhotoDto;
-import aespa.groovymap.mypage.dto.MyPagePhoto.MyPagePhotoCommentDto;
 import aespa.groovymap.mypage.dto.MyPagePhoto.MyPagePhotoWriteDto;
 import aespa.groovymap.mypage.dto.MyPagePhoto.MyPagePhotosDto;
 import aespa.groovymap.mypage.service.MyPagePhotoService;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
@@ -61,64 +59,6 @@ public class MyPagePhotoController {
         log.info("마이 페이지 게시글 삭제 요청 : {}", postId);
         if (memberId != null) {
             myPagePhotoService.deleteMyPagePhoto(memberId, postId);
-            return ResponseEntity.ok("");
-        }
-        return ResponseEntity.badRequest().body("need login");
-    }
-
-    @PostMapping("/mypage/photo/{postId}/like")
-    public ResponseEntity likeMyPagePhoto(
-            @SessionAttribute(name = SessionConstants.MEMBER_ID, required = false) Long memberId,
-            @PathVariable("postId") Long postId) {
-        log.info("마이 페이지 게시글 좋아요 요청, 요청 member : {}", memberId);
-        if (memberId != null) {
-            Boolean likedSuccess = myPagePhotoService.likeMyPagePhoto(memberId, postId);
-
-            if (likedSuccess) {
-                return ResponseEntity.ok("post like success");
-            } else {
-                return ResponseEntity.badRequest().body("already liked post");
-            }
-        }
-        return ResponseEntity.badRequest().body("need login");
-    }
-
-    @PostMapping("/mypage/photo/{postId}/unlike")
-    public ResponseEntity unlikeMyPagePhoto(
-            @SessionAttribute(name = SessionConstants.MEMBER_ID, required = false) Long memberId,
-            @PathVariable("postId") Long postId) {
-        log.info("마이 페이지 게시글 좋아요 취소 요청, 요청 member : {}", memberId);
-        if (memberId != null) {
-            Boolean likedSuccess = myPagePhotoService.unlikeMyPagePhoto(memberId, postId);
-
-            if (likedSuccess) {
-                return ResponseEntity.ok("post unlike success");
-            } else {
-                return ResponseEntity.badRequest().body("not liked post");
-            }
-        }
-        return ResponseEntity.badRequest().body("need login");
-    }
-
-    @PostMapping("/mypage/photo/{postId}/comments")
-    public ResponseEntity writeComment(
-            @SessionAttribute(name = SessionConstants.MEMBER_ID, required = false) Long memberId,
-            @PathVariable("postId") Long postId, @RequestBody MyPagePhotoCommentDto myPagePhotoCommentDto) {
-
-        log.info("마이 페이지 게시글에 댓글 요청, 작성자 id : {}, 게시글 id : {}", memberId, postId);
-
-        myPagePhotoService.writeComment(memberId, postId, myPagePhotoCommentDto.getText());
-
-        return ResponseEntity.ok("");
-    }
-
-    @DeleteMapping("/mypage/photo/{postId}/comments/{commentId}")
-    public ResponseEntity deleteComment(
-            @SessionAttribute(name = SessionConstants.MEMBER_ID, required = false) Long memberId,
-            @PathVariable("commentId") Long commentId) {
-        log.info("마이 페이지 게시글 댓글 삭제 요청 : {}", commentId);
-        if (memberId != null) {
-            myPagePhotoService.deleteMyPagePhotoComment(memberId, commentId);
             return ResponseEntity.ok("");
         }
         return ResponseEntity.badRequest().body("need login");
--- src/main/java/aespa/groovymap/mypage/service/MyPagePhotoService.java
+++ src/main/java/aespa/groovymap/mypage/service/MyPagePhotoService.java
@@ -3,7 +3,6 @@ package aespa.groovymap.mypage.service;
 import aespa.groovymap.domain.Comment;
 import aespa.groovymap.domain.Member;
 import aespa.groovymap.domain.MemberContent;
-import aespa.groovymap.domain.post.LikedPost;
 import aespa.groovymap.domain.post.MyPagePost;
 import aespa.groovymap.domain.post.Post;
 import aespa.groovymap.mypage.dto.MyPagePhoto.MyPageOnePhotoDto;
@@ -12,8 +11,6 @@ import aespa.groovymap.mypage.dto.MyPagePhoto.MyPagePhotoDto;
 import aespa.groovymap.mypage.dto.MyPagePhoto.MyPagePhotoWriteDto;
 import aespa.groovymap.mypage.dto.MyPagePhoto.MyPagePhotosDto;
 import aespa.groovymap.mypage.repository.MyPagePostRepository;
-import aespa.groovymap.repository.CommentRepository;
-import aespa.groovymap.repository.LikedPostRepository;
 import aespa.groovymap.repository.MemberRepository;
 import aespa.groovymap.repository.PostRepository;
 import aespa.groovymap.uploadutil.util.FileUpload;
@@ -37,8 +34,6 @@ public class MyPagePhotoService {
     private final MyPagePostRepository myPagePostRepository;
     private final MemberRepository memberRepository;
     private final PostRepository postRepository;
-    private final CommentRepository commentRepository;
-    private final LikedPostRepository likedPostRepository;
     private final FileUpload fileUpload;
 
     public MyPagePhotosDto getMyPagePhotos(String nickname) {
@@ -174,91 +169,5 @@ public class MyPagePhotoService {
             throw new NoSuchElementException("Wrong post id, you need to send MyPagePhotoId");
         }
         postRepository.delete(post);
-    }
-
-    public void writeComment(Long memberId, Long postId, String text) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NoSuchElementException("Wrong Member Id"));
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new NoSuchElementException("Wrong Post Id"));
-
-        Comment comment = makeComment(text, member, post);
-        post.getComments().add(comment);
-    }
-
-    private Comment makeComment(String text, Member member, Post post) {
-        Comment comment = new Comment();
-        comment.setCommentAuthor(member);
-        comment.setContent(text);
-        comment.setCommentPost(post);
-        comment.setTimestamp(ZonedDateTime.now());
-
-        commentRepository.save(comment);
-        return comment;
-    }
-
-    public void deleteMyPagePhotoComment(Long memberId, Long commentId) {
-        Comment comment = commentRepository.findById(commentId)
-                .orElseThrow(() -> new NoSuchElementException("Wrong Comment Id"));
-
-        if (comment.getCommentAuthor().getId() == memberId) {
-            commentRepository.delete(comment);
-        } else {
-            throw new SecurityException("cannot delete other user's post");
-        }
-    }
-
-    public Boolean likeMyPagePhoto(Long memberId, Long postId) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NoSuchElementException("Wrong Member Id"));
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new NoSuchElementException("Wrong Post Id"));
-
-        Boolean isLikedPost = isLikedPost(member.getMemberContent(), postId);
-
-        if (!isLikedPost) {
-            likedPostLogic(member, post);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private void likedPostLogic(Member member, Post post) {
-        LikedPost likedPost = makeLikedPost(member, post);
-        member.getMemberContent().getLikedPosts().add(likedPost);
-        post.increaseLikesCount();
-
-        likedPostRepository.save(likedPost);
-    }
-
-    private LikedPost makeLikedPost(Member member, Post post) {
-        LikedPost likedPost = new LikedPost();
-        likedPost.setLikedMemberContent(member.getMemberContent());
-        likedPost.setLikedPost(post);
-        return likedPost;
-    }
-
-
-    public Boolean unlikeMyPagePhoto(Long memberId, Long postId) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NoSuchElementException("Wrong Member Id"));
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new NoSuchElementException("Wrong Post Id"));
-
-        MemberContent memberContent = member.getMemberContent();
-        List<LikedPost> likedPosts = memberContent.getLikedPosts();
-
-        boolean removed = likedPosts.removeIf(likedPost -> {
-            if (likedPost.getLikedPost().getId().equals(postId)) {
-                likedPostRepository.delete(likedPost);
-                post.decreaseLikesCount();
-                return true;
-            } else {
-                return false;
-            }
-        });
-
-        return removed;
     }
 }
