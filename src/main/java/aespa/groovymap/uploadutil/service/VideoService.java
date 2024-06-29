6f92f6b7d26a711966ba73ab91ada3b1bb22f2b6
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-06-29 19:52:33 +0900

    Feat : 영상 요청 분리
    파일 응답할 때 이미지와 영상 응답 분리
    영상 응답할 때 다른 방식으로 응답해야 함
    따라서 파일 저장할 때에도 영상인 경우 url 주소 /videos/로 함

---
+++ src/main/java/aespa/groovymap/uploadutil/service/VideoService.java
@@ -0,0 +1,32 @@
+package aespa.groovymap.uploadutil.service;
+
+import java.io.IOException;
+import java.util.Optional;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.ResourceRegion;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRange;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class VideoService {
+    
+    public ResourceRegion getMp4ResourceRegion(UrlResource video, HttpHeaders headers) throws IOException {
+        final long chunkSize = 1000000L;
+        long contentLength = video.contentLength();
+        Optional<HttpRange> optional = headers.getRange().stream().findFirst();
+        if (optional.isPresent()) {
+            HttpRange httpRange = optional.get();
+            long start = httpRange.getRangeStart(contentLength);
+            long end = httpRange.getRangeEnd(contentLength);
+            long rangeLength = Long.min(chunkSize, end - start + 1);
+            return new ResourceRegion(video, start, rangeLength);
+        } else {
+            long rangeLength = Long.min(chunkSize, contentLength);
+            return new ResourceRegion(video, 0, rangeLength);
+        }
+    }
+
+}
