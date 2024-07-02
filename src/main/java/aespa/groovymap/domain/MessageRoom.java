528e1969bf35acfa4907c7a109eaa05a63f9d7ac
YJK-dev <90750483+YJK-dev@users.noreply.github.com>
2024-07-02 18:32:04 +0900

    Feat: Message 및 MessageRoom 엔티티 확장 및 수정
    - Message 엔티티에 receiver와 sender 필드 추가
    - 메시지 읽음 상태를 나타내는 isRead 필드 추가
    
    - MessageRoom 엔티티의 필드 매핑 수정:
      - sender 필드 @JoinColumn(name = "sender_id")로 수정
      - receiver 필드 @JoinColumn(name = "receiver_id")로 수정

--- src/main/java/aespa/groovymap/domain/MessageRoom.java
+++ src/main/java/aespa/groovymap/domain/MessageRoom.java
@@ -5,28 +5,33 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "sender_id")
+    @JoinColumn(name = "receiver_id")
     private Member receiver;
 
     @ManyToOne
-    @JoinColumn(name = "receiver_id")
+    @JoinColumn(name = "sender_id")
     private Member sender;
 
     @OneToMany(mappedBy = "messageRoom", cascade = CascadeType.ALL, orphanRemoval = true)
