f84dfddaf53c1daa95c06fe2e9788d5d1d8aec58
CJ-1998 <96980115+CJ-1998@users.noreply.github.com>
2024-08-12 22:56:09 +0900

    [준] CI/CD 적용 및 소셜 로그인 구현 및 사용 안하는 빈 관련 오류 수정 (#14)
    * Feat : db 설정 수정
    
    RDS에 설정한 DB로 application.properties 수정
    
    * Feat : EC2 https 설정 위한 api 추가
    
    로드밸런서 상태 체크 위한 api 추가
    
    * Feat : application.properties 수정
    
    쿼리문 안 보이게 수정
    
    * Feat : 로그 추가
    
    필요한 컨트롤러 및 서비스에 로그 추가
    
    * Chore : application.properties 개발 배포 분리
    
    application.properties 개발 배포 분리
    application.properties가 개발 환경
    application-prod.properties가 배포 환경
    
    * Chore : CI/CD 적용
    
    Github Action을 이용한 CI/CD 적용
    CI/CD 적용 위해 .github/workflows 폴더 생성
    그 후 폴더에 CI, CD 위한 yaml 파일 생성
    
    * Feat : application.properties 수정
    
    개발, 배포 환경 분리 위해 수정
    
    * Feat : 카카오 로그인위한 클래스 생성
    
    카카오 로그인 위한 기본 클래스들 생성
    
    * Feat : application.properties 수정
    
    properties 파일 include 하는 것은 application.properties에서만 해야 함
    
    * Chore : 깃허브의 파일 삭제
    
    깃허브에 올라간 파일 삭제 후 gitignore 적용
    
    * Feat : 카카오 로그인 위한 설정 추가
    
    application.properties 및 application-kakao.properties 수정 및 생산
    
    * Feat : 카카오 소셜 로그인 기능 구현
    
    카카오 소셜 로그인 기능 구현 완료
    
    * Feat : 구글 로그인 위한 기본 구현 완료
    
    구글 로그인 위한 설정 및 기본 구현 완료
    
    * Feat : 구글 소셜 로그인 구현
    
    구글 소셜 로그인 구현 완료
    OAuthService의 메서드들 소셜 로그인 함께 사용할 수 있도록 리팩토링함
    
    * Chore : CD 파일 수정
    
    브랜치 이름 main에서 master로 수정
    
    * Fix : 사용 안하는 빈들 제거
    
    로컬에서 테스트할 때 사용하던 빈들 사용 안하는 것으로 처리

--- src/main/java/aespa/groovymap/uploadutil/util/LocalFileUpload.java
+++ src/main/java/aespa/groovymap/uploadutil/util/LocalFileUpload.java
@@ -3,12 +3,10 @@ package aespa.groovymap.uploadutil.util;
 import java.io.File;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-@Component
-@PropertySource("classpath:application-file.properties")
+//@Component
+//@PropertySource("classpath:application-file.properties")
 public class LocalFileUpload implements FileUpload {
 
     @Value("${file.dir}")
